1f99798261a7725cc9ab0ed40f1121d805837061
Pierre Huchant <pierre.huchant@inria.fr>
2017-10-10 19:15:41 +0200

    libsplit: Handle different versions of clang/LLVM.

--- libsplit/src/Define.h
+++ libsplit/src/Define.h
@@ -3,22 +3,28 @@
 
 namespace libsplit {
 
-#define OPENCLFLAGS "-cc1 -cl-std=CL1.2 "		\
-  "-emit-llvm-bc "				\
-  "-isystem " LLVM_LIB_DIR "/clang/3.9.1/include/ -finclude-default-header" \
+#if CLANGMAJOR >= 5
+#define OPENCLFLAGS "-Xclang -finclude-default-header -c -emit-llvm "	\
+  "-cl-std=CL1.2"
+
+#define OPTPASSES "-O2"
+#else
+#define OPENCLFLAGS "-cc1 -cl-std=CL1.2 "              \
+  "-emit-llvm-bc "				       \
+  "-finclude-default-header"			       \
   " -O0"
 
-#define OPTPASSES							\
-  "-targetlibinfo -tbaa -basicaa -globalopt -ipsccp -deadargelim " \
+#define OPTPASSES                                                      \
+  "-targetlibinfo -tbaa -basicaa -globalopt -ipsccp -deadargelim "	\
   "-simplifycfg -basiccg -prune-eh -always-inline -functionattrs -sroa " \
-  "-domtree -early-cse -lazy-value-info -jump-threading " \
+  "-domtree -early-cse -lazy-value-info -jump-threading "		\
   "-correlated-propagation -simplifycfg -tailcallelim -simplifycfg "	\
   "-reassociate -domtree -loops -loop-simplify -lcssa -loop-rotate -licm " \
   "-lcssa -loop-unswitch -scalar-evolution -loop-simplify -lcssa -indvars " \
   "-loop-idiom -loop-deletion -loop-unroll -memdep -memcpyopt -sccp "	\
   "-lazy-value-info -jump-threading -correlated-propagation -domtree -memdep " \
   "-dse -adce -simplifycfg -strip-dead-prototypes -domtree -loop-reduce -verify"
-
+#endif
 
 #define PASSARG "-klanalysis"
 
